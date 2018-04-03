1509558aa7260b114410621431e1a9a25df824b5
Abby <arwren@wpi.edu>
2018-04-03 11:13:58 -0400

    I added specific ids to the text views and buttons for the two xmls for the activities  I attempted to make an intent for one of the buttons in the main activity java

--- app/src/main/java/com/example/zeynepalptekin/tariccostechsavvycodingnightmares/MainActivity.java
+++ app/src/main/java/com/example/zeynepalptekin/tariccostechsavvycodingnightmares/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.zeynepalptekin.tariccostechsavvycodingnightmares;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent1 = new Intent(this, CreateListing.class);
+        Button button1 = findViewById(R.id.createListingButton);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+
+                startActivity(intent1);
+            }
+        });
     }
 }
+
+

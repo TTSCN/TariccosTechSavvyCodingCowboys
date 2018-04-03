504ec557ae4d8168aae8e927325a7a9612042fde
Zeynep Alptekin <zeynep.e.alptekin@hotmail.com>
2018-04-03 11:05:16 -0400

    Added potential image selector to create listing class plan: do image and "listing" separate want to find way to add image object to array TODO: add code for filling in listing array in createListing

--- app/src/main/java/com/example/zeynepalptekin/tariccostechsavvycodingnightmares/CreateListing.java
+++ app/src/main/java/com/example/zeynepalptekin/tariccostechsavvycodingnightmares/CreateListing.java
@@ -1,5 +1,6 @@
 package com.example.zeynepalptekin.tariccostechsavvycodingnightmares;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,20 @@ public class CreateListing extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_listing);
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+    }
+
+    public static final int PICK_IMAGE = 1;
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == PICK_IMAGE) {
+            //TODO: action
+        }
     }
 }

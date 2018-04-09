6d9d2f334f208501a2bc76da6a328ea30b80fff2
Zeynep Alptekin <zeynep.e.alptekin@hotmail.com>
2018-04-09 09:04:40 -0400

    fixed merge wrote code to grab image added image view to create listing activity changed tools:context of create listing activity to be create_listing

--- app/src/main/java/com/example/zeynepalptekin/tariccostechsavvycodingnightmares/Create_Listing.java
+++ app/src/main/java/com/example/zeynepalptekin/tariccostechsavvycodingnightmares/Create_Listing.java
@@ -1,10 +1,17 @@
 package com.example.zeynepalptekin.tariccostechsavvycodingnightmares;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class Create_Listing extends AppCompatActivity {
+    public static final int PICK_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +24,24 @@ public class Create_Listing extends AppCompatActivity {
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
     }
 
-    public static final int PICK_IMAGE = 1;
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data)
     {
-        if (requestCode == PICK_IMAGE) {
-            //TODO: action
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 

0f04252966c9eb8dee34f06b9dbea4d4e6191210
Abby <arwren@wpi.edu>
2018-04-17 14:23:22 -0400

    I copied and adjusted the labels from the CreateListingService.xml to the CreateListingEquipment.xml. I connected the addImage code to the addImage button in both createListing classes

--- app/src/main/java/com/example/zeynepalptekin/tariccostechsavvycodingnightmares/CreateListingService.java
+++ app/src/main/java/com/example/zeynepalptekin/tariccostechsavvycodingnightmares/CreateListingService.java
@@ -1,18 +1,12 @@
 package com.example.zeynepalptekin.tariccostechsavvycodingnightmares;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.io.IOException;
 
 public class CreateListingService extends AppCompatActivity {
     public static final int PICK_IMAGE = 1;
@@ -22,11 +16,6 @@ public class CreateListingService extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_listing_service);
 
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
-
         Button backToMain = findViewById(R.id.backToMain7);
         backToMain.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
@@ -34,6 +23,12 @@ public class CreateListingService extends AppCompatActivity {
             }
         });
 
+        Button addImage = findViewById(R.id.addImageButton1);
+        addImage.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                backToMain();
+            }
+        });
 
     }
 
@@ -75,7 +70,7 @@ public class CreateListingService extends AppCompatActivity {
         str = text.getText().toString();
         serviceL.setCost(Double.parseDouble(str));
 
-        text = findViewById(R.id.descriptionText);
+        text = findViewById(R.id.descriptionText2);
         str = text.getText().toString();
 
         str = text.getText().toString();
@@ -85,5 +80,12 @@ public class CreateListingService extends AppCompatActivity {
     public void backToMain() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void addImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
     }
 }

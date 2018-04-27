efe1e3601cd5fbf5014764fa9075ae2064118c1c
Magnus-Tryggvi <magnustry@gmail.com>
2018-04-27 09:41:51 -0400

    ChildEventListener set up, will update code in each method later

--- app/src/main/java/com/example/zeynepalptekin/tariccostechsavvycodingnightmares/ListingsView.java
+++ app/src/main/java/com/example/zeynepalptekin/tariccostechsavvycodingnightmares/ListingsView.java
@@ -10,6 +10,12 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 import java.util.Currency;
 
